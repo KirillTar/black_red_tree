a98fc68fbaa4b0299668d4ee1ab33aaecd2dc01c
Kirill Tarazanov <killtarr@gmail.com>
2024-10-17 16:46:09 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
 #include "App.h"
 
-template <typename T>
-class A {
-	
-	A() {
-	}
-
-public:
-	static A(T t) {
-		x = t;
-	}
-
-	T x;
-};
-
-A a(1.0);
-
 int main(int argc, char* argv[]) {
 
 	Application app;
